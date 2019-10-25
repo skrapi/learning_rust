307e176344f6bc44d31aae238237e7772173b5ce
sylvan <sylvan@plentify.co.za>
2019-10-25 13:35:52 +0200

    replaced with struct

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+  rectangle.width * rectangle.height
 }
