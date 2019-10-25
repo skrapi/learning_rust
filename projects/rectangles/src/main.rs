643a2cb23000c5869a043e69280ffefaf5013df0
sylvan <sylvan@plentify.co.za>
2019-10-25 15:56:15 +0200

    Done playing around with structs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,13 +4,32 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
     
-    println!("rect1 is {:#?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-  rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+    let square = Rectangle::square(10);
+
+    println!("The area of the square is {}", square.area());
+
+    println!("rect1 is {:#?}", rect1);
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
+
