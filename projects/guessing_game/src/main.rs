19d9eca78d00e11b09859dceaa69ce062b9e6d53
sylvan <sylvan@plentify.co.za>
2019-10-26 16:45:33 +0200

    Added struct for guess

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -2,6 +2,26 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
   println!("Guess the number!");
     
@@ -20,14 +40,11 @@ fn main() {
       Err(_) => continue,
     };
 
-    if guess < 1 || guess > 100 {
-        println!("The secret number will be between 1 and 100.");
-        continue;
-    }
+    let guess = Guess::new(guess);
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess.value);
 
-    match guess.cmp(&secret_number) {
+    match guess.value.cmp(&secret_number) {
       Ordering::Less => println!("Too low!"),
       Ordering::Greater => println!("Too high!"),
       Ordering::Equal => {
