c868d7fa397e0b653a2c68b3bb27fa17df2549f4
sylvan <sylvan@plentify.co.za>
2019-10-27 20:33:56 +0200

    Replaced the for loops with iterators

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -9,13 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -33,7 +38,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let results = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
-        searc_case_insensitive(&config.query, &contents)
+        search_case_insensitive(&config.query, &contents)
     };
 
     for line in results {
@@ -44,28 +49,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn searc_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
@@ -95,7 +89,7 @@ Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            searc_case_insensitive(query, contents)
+            search_case_insensitive(query, contents)
         );
     }
 }
